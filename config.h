262ed08004491e90145c58d8113b0ac241a1586e
Leonardo Rossi <lrossi.dev@pm.me>
2021-12-10 08:58:27 -0300

    Switching colors to dracula theme

--- config.h
+++ config.h
@@ -105,28 +105,51 @@ float alpha = 1;
 // #include "/home/lrossi/.cache/wal/colors-wal-st.h"
  /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
+	// "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	// "#cc241d",
+	// "#98971a",
+	// "#d79921",
+	// "#458588",
+	// "#b16286",
+	// "#689d6a",
+	// "#a89984",
+	// "#928374",
+	// "#fb4934",
+	// "#b8bb26",
+	// "#fabd2f",
+	// "#83a598",
+	// "#d3869b",
+	// "#8ec07c",
+	// "#ebdbb2",
+	// [255] = 0,
+	// /* more colors can be added after 255 to use with DefaultXX */
+	// "#add8e6", /* 256 -> cursor */
+	// "#555555", /* 257 -> rev cursor*/
+	// "#282828", /* 258 -> bg */
+	// "#ebdbb2", /* 259 -> fg */
+
+	"#282a36", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	"#ff5555",
+	"#50fa7b",
+	"#f1fa8c",
+	"#bd93f9",
+	"#ff79c6",
+	"#8be9fd",
+	"#f8f8f2",
+	"#44475a", 
+	"#ff5555",
+	"#50fa7b",
+	"#f1fa8c",
+	"#bd93f9",
+	"#ff79c6",
+	"#8be9fd",
+	"#f8f8f2",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
+	"#f8f8f2", /* 256 -> cursor */
+	"#44475a", /* 257 -> rev cursor*/
+	"#282a36", /* 258 -> bg */
+	"#f8f8f2", /* 259 -> fg */
 };
 
 
